f3cfd3dbb52c958a8812a7bdaefc0cf87273e48a
Peter <murdzhevpetar@gmail.com>
2025-12-19 23:28:34 +0200

    product page changes

--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -1,12 +1,13 @@
 import ProductUI from "@/components/ProductUI";
 
-export default async function page({params}) {
-  const {id} = await params;
-  const product = await fetch(`https://dummyjson.com/products/${id}`)
-            .then(response => response.json())
-            .then(data => data);
+export default async function page({ params }) {
+  const { id } = await params;
+  const product = await fetch(`https://dummyjson.com/products/${id}`, {
+    next: { revalidate: 0 }
+  })
+    .then(response => response.json())
 
   return (
-    <ProductUI product={product}/>
+    <ProductUI product={product} />
   )
 }
