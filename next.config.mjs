6d2c630de45699c19e7b52d9e37e0dad8d67d71e
Peter <murdzhevpetar@gmail.com>
2025-12-19 23:13:19 +0200

    dynamic added

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     allowedDevOrigins: ['http://10.52.42.65:3000'],
     eslint: {
         ignoreDuringBuilds: true,
+    },
+    experimental: {
+        serverActions: true,
     }
 };
 
--- src/app/_not-found/layout.js
+++
@@ -1,6 +0,0 @@
-export default function NotFoundLayout({ children }) {
-    return (
-        <html lang="en">
-            <body> {children} </body>
-        </html>);
-}
